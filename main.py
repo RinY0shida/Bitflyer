d504f390324b5b5855c516ae418ed15298b160ca
RinY0shida <tororo1219@gmail.com>
2024-12-05 16:57:16 +0900

    ディレクトリ構造の見直し

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     bitcoin_ohcl_fetcher = BitcointOhclFetcher()
     
     minute_ohcl_result = bitcoin_ohcl_fetcher.FetchOhclData("histominute")
+    hour_ohcl_result = bitcon
     print(f"取得した分速データの個数: {len(minute_ohcl_result)}個")
 
 if __name__ == "__main__":
