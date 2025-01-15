dd722c72de498dce1a9bed3008066e0090fd01b1
RinY0shida <tororo1219@gmail.com>
2025-01-15 21:42:10 +0900

    add GetAccountInformation function

--- bitflyer_trade.py
+++ bitflyer_trade.py
@@ -11,18 +11,18 @@ class BitflyerTrade:
     # @brief 成り行き買い注文
     # @param amount 注文数量(BTC)
     # @param expiration_time 有効期限(minute)
-    def BuyMkt(amount, expiration_time):
+    def BuyMkt(self, amount, expiration_time):
         amount = int(amount*100000000)/100000000
-        buy = self.api.sendchildorder(product_code = "BTC_JPY", child_order_type = "MARKET", side = "BUY", amount, expiration_time, time_in_force = "GTC")
+        buy = self.api.sendchildorder(product_code = "BTC_JPY", child_order_type = "MARKET", side = "BUY", amount = amount, minute_to_expire = expiration_time, time_in_force = "GTC")
         print("BuyMkt: ", amount)
         print(buy)
 
     # @brief 成り行き売り注文
     # @param amount 注文数量(BTC)
     # @param expiration_time 有効期限(minute)
-    def SellMkt(amount, expiration_time):
+    def SellMkt(self, amount, expiration_time):
         amount = int(amount*100000000)/100000000
-        sell = self.api.sendchildorder(product_code = "BTC_JPY", child_order_type = "MARKET", side = "SELL", amount, expiration_time, time_in_force = "GTC")
+        sell = self.api.sendchildorder(product_code = "BTC_JPY", child_order_type = "MARKET", side = "SELL", amount = amount, minute_to_expire = expiration_time, time_in_force = "GTC")
         print("SellMkt: ", amount)
         print(sell)
 
@@ -30,10 +30,10 @@ class BitflyerTrade:
     # @param amount 注文数量(BTC)
     # @param price 注文価格(JPY)
     # @param expiration_time 有効期限(minute)
-    def BuyLmt(amount, price, expiration_time):
+    def BuyLmt(self, amount, price, expiration_time):
         amount = int(amount*100000000)/100000000
         price = int(price)
-        buy = self.api.sendchildorder(product_code = "BTC_JPY", child_order_type = "LIMIT", side = "BUY", price, amount, expiration_time, time_in_force = "GTC")
+        buy = self.api.sendchildorder(product_code = "BTC_JPY", child_order_type = "LIMIT", side = "BUY", price = price, amount = amount, minute_to_expire = expiration_time, time_in_force = "GTC")
         print("BuyLmt: ", amount, price)
         print(buy)
 
@@ -41,13 +41,13 @@ class BitflyerTrade:
     # @param amount 注文数量(BTC)
     # @param price 注文価格(JPY)
     # @param expiration_time 有効期限(minute)
-    def SellLmt(amount, price, expiration_time):
+    def SellLmt(self, amount, price, expiration_time):
         amount = int(amount*100000000)/100000000
         price = int(price)
-        sell = self.api.sendchildorder(product_code = "BTC_JPY", child_order_type = "LIMIT", side = "SELL", price, amount, expiration_time, time_in_force = "GTC")
+        sell = self.api.sendchildorder(product_code = "BTC_JPY", child_order_type = "LIMIT", side = "SELL", price = price, amount = amount, minute_to_expire = expiration_time, time_in_force = "GTC")
         print("SellLmt: ", amount, price)
         print(sell)
 
-    def GetAccountImformation():
+    def GetAccountInformation(self):
         account = self.api.getbalance()
         print(account)
