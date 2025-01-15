01e4a98f23ceb2a99f2862fe947a4db189aa03a1
RinY0shida <tororo1219@gmail.com>
2025-01-15 21:43:02 +0900

    test bitflyer_trade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 from bitcoin_ohcl_fetcher import BitcointOhclFetcher
+from bitflyer_trade import BitflyerTrade
 
 # @brief APIキーを取得する
 # @param file_name APIキーが記載されたファイル名
@@ -17,7 +18,7 @@ def main():
     
     minute_ohcl_result = bitcoin_ohcl_fetcher.FetchOhclData("histominute", "1999")
     #print(f"取得した分速データの個数: {len(minute_ohcl_result)}個")
-    #rint(minute_ohcl_result[:2000])
+    #print(minute_ohcl_result[:2000])
     
     minute_close = [data["close"] for data in minute_ohcl_result]
     minute_average = sum(minute_close) / len(minute_close)
@@ -31,6 +32,9 @@ def main():
     key_data = GetApiKey("bitflyer_api_key.json")
     print(key_data["api_key"])
     print(key_data["api_secret"])
+
+    bitflyer_trade = BitflyerTrade(key_data["api_key"], key_data["api_secret"])
+    bitflyer_trade.GetAccountInformation()
 
 
 
